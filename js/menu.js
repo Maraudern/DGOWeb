78dc2b253f03a2970d1704267996c03c81190edb
Maraudern <2955166213@qq.com>
2021-10-24 20:14:14 +0800

    new

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,12 @@
 /* btnReal JS Document */
 
 //菜单背景
-$(document).ready(function(){
-    $("nav").addClass("navScroll");
-});
-
 $(window).scroll(function() {
-  if ($(document).scrollTop()<=winHeight-50){
-    $("nav").addClass("navScroll");
+  if ($(document).scrollTop()<=winHeight/2){
+    $("nav").removeClass("navScroll");
   }
   else{
-    $("nav").removeClass("navScroll");
+    $("nav").addClass("navScroll");
   }
 });
 
